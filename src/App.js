d112745bfb9f38819a5f3d62054acd7446add2b5
Vanessa Di Bella <vanessa@di-bella.de>
2024-07-01 10:57:15 +0200

    add api and fetch

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import useLocalStorageState from "use-local-storage-state";
 import Form from "./components/Form";
 import List from "./components/List";
 
+import { useEffect, useState } from "react";
+
 export default function App() {
+  const [weather, setWeather] = useState({});
   const [activities, setActivities] = useLocalStorageState("activities", {
     defaultValue: [],
   });
-
-  const isGoodWeather = true;
-  // const [activities, setActivities] = useState([])
+  const isGoodWeather = weather.isGoodWeather;
   function handleAddActivity(newActivity) {
     setActivities([...activities, newActivity]);
     console.log(activities);
@@ -18,8 +19,23 @@ export default function App() {
       ? activity.isForGoodWeather
       : !activity.isForGoodWeather;
   });
+
+  useEffect(() => {
+    async function startFetching() {
+      const response = await fetch(
+        "https://example-apis.vercel.app/api/weather/europe"
+      );
+      const weather = await response.json();
+      setWeather(weather);
+    }
+    startFetching();
+  }, []);
+
   return (
     <>
+      <h1>
+        {weather.condition} {weather.temperature} °C
+      </h1>
       <List activities={filterActivities} isGoodWeather={isGoodWeather} />
       <Form onAddActivity={handleAddActivity} />
     </>
